e9426c52728f05bdf55bcd0c8e6c5ea479347c23
Johannes <106240360+hannesdevs@users.noreply.github.com>
2022-05-26 00:40:06 +0200

    made some explanations

--- index.js
+++ index.js
@@ -17,17 +17,20 @@ const client = new Discord.Client({
 //  console.log (duh)
 
 client.on("ready", () => {
-    console.log(`Logget inn som ${client.user.tag}`)
+    console.log(`Logged in as ${client.user.tag}`)
 })
 
+// little config
 const welcomeChannelId = "978969837495779348"
 
+// sends the welcome message
 client.on("guildMemberAdd", async (member) => {
     const img = await generateImage(member)
     member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to the server!`,
+        content: `<@${member.id}> Welcome to the server!`, // remove this line of code if you dont want the bot to say "Welcome to the Server" along with the image.
         files: [img]
     })
 })
 
+// dosnt have to explain
 client.login(process.env.TOKEN)
