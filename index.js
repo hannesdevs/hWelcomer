fffcbb853b77e2d2a7785112c67d2776087f1c99
hannesdevs <johannes.bingen@gmail.com>
2022-05-25 13:10:53 +0200

    Dosn't fully work, and i do no longer maintaince this.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const Discord = require('discord.js')
 require("dotenv").config()
 
-const TOKEN = "OTc4OTU3MTAyOTk3NDQyNjEw.GR8J5W.nLRiEz65japKyXjI_-_Mbwgen-mK75hEE9Sf0o"
-
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -17,26 +15,6 @@ const client = new Discord.Client({
 
 client.on("ready", () => {
     console.log(`Logget inn som ${client.user.tag}`)
-})
-
-// noen hyggelige tilbakemeldinger
-
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello! How are you?")
-    }
-})
-
-client.on("messageCreate", (message) => {
-    if (message.content == "im good"){
-        message.reply("Good too hear!")
-    }
-})
-
-client.on("messageCreate", (message) => {
-    if (message.content == "im fine"){
-        message.reply("Good too hear!")
-    }
 })
 
 client.login(process.env.TOKEN)
