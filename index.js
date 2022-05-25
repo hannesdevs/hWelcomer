277c7e23f080729f3ff7943542abc9f270fa2859
hannesdevs <johannes.bingen@gmail.com>
2022-05-25 13:11:46 +0200

    Dosn't fully work, and i do no longer maintaince this.

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 const Discord = require('discord.js')
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -15,6 +18,16 @@ const client = new Discord.Client({
 
 client.on("ready", () => {
     console.log(`Logget inn som ${client.user.tag}`)
+})
+
+const welcomeChannelId = "978969837495779348"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
